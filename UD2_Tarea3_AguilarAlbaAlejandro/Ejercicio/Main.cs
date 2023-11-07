4191bd8ec3cb8125f60af878af4e83b623c56ee8
aaguaalb451 <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>
2023-11-07 12:49:52 +0100

    mejoras

---
+++ UD2_Tarea3_AguilarAlbaAlejandro/Ejercicio/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UD2_Tarea3_AguilarAlbaAlejandro.Tarea3;
+
+namespace UD2_Tarea3_AguilarAlbaAlejandro.Ejercicio {
+    public class MainClass {
+        private const int SALIR = 0;
+        private const int RECTANGULO = 1;
+        private const int CIRCULO = 2;
+        private const int TRIANGULO = 3;
+        private const int MAXIMO = 3;
+        private Pedirdatos per= new Pedirdatos();
+        private const string TEXT_MENU="----MENU---";
+        private string TEXT_SALIDA = String.Format("{0} -> Salida del programa",SALIR);
+        private string TEXT_RECTANGULO = String.Format( "{0} -> Uso del rectángulo", RECTANGULO );
+        private string TEXT_CIRCULO = String.Format( "{0} -> Uso del círculo", CIRCULO );
+        private string TEXT_TRIANGULO = String.Format( "{0} -> Uso del círculo", TRIANGULO );
+
+        private void Tarea3() {
+            Menu();
+        }
+
+        private void TEXTMENU() {
+            Console.WriteLine( TEXT_MENU );
+            Console.WriteLine( TEXT_SALIDA );
+            Console.WriteLine( TEXT_RECTANGULO );
+            Console.WriteLine( TEXT_CIRCULO );
+            Console.WriteLine( TEXT_TRIANGULO );
+        }
+        private void Menu() {
+            bool salida = false;
+            Console.WriteLine("Programa sobre las figuras geométricas");
+            TEXTMENU();
+            do {
+                switch ( per.PedirIntEnRango( SALIR, MAXIMO ) ) {
+                    case SALIR:
+                        salida = true;
+                        break;
+                    case RECTANGULO:
+                        Console.Write( "b" );
+                        break;
+                    case CIRCULO:
+                        Console.Write( "c" );
+                        break;
+                    case TRIANGULO:
+                        Console.Write( "" );
+                        break;
+                }
+            } while (!salida );
+            Console.Write( "Salida del programa con exito" );
+        }
+        public static void Main( string[] args ) {
+            new MainClass().Tarea3();
+        }
+    }
+}
---
+++ UD2_Tarea3_AguilarAlbaAlejandro/Ejercicio/Pedirdatos.cs
@@ -0,0 +1,154 @@
+﻿using System;
+namespace UD2_Tarea3_AguilarAlbaAlejandro.Tarea3 {
+    public class Pedirdatos {
+        public bool Pedirbool(String linea1,String linea2) {
+            bool salida = false;
+            bool resultado = false; string frase;
+            do {
+                Console.Write( "Escribe un {0} o un {1} \n",linea1,linea2 );
+                frase = PedirString();
+                if ( frase.Equals( linea1 ) ) {
+                    salida = true;
+                    resultado = true;
+                } else if ( frase.Equals( linea2 ) ) {
+                    salida = true;
+                    resultado = false;
+                } else {
+                    Console.Write( "Escribe bien el si o no\n" );
+                }
+            } while ( !salida );
+            return resultado;
+        }
+
+        internal int PedirIntEnRango() {
+            throw new NotImplementedException();
+        }
+
+        public String PedirString() {
+            bool salida = false;
+            String frase;
+            do {
+                Console.Write( "\n->" );
+                frase = Console.ReadLine().Trim();
+                if ( frase.Length > 0 ) {
+                    salida = true;
+                } else {
+                    Console.Write( "Escribe algo\n" );
+                }
+            } while ( !salida );
+            return frase;
+        }
+        public String PedirStringSinControl() {
+            Console.Write( "\n->" );
+            return Console.ReadLine().Trim();
+        }
+        public String PedirStringSinControl(string frase) {
+            Console.Write( frase );
+            return PedirStringSinControl();
+        }
+        public String PedirString(string muestra) {
+            Console.Write( muestra );
+            return PedirString();
+        }
+
+        public int PedirIntEnRango(int num, int num2) {
+            bool salida = false;
+            int numero1;
+            int numero2;
+            if (num>=num2) {
+                numero1 = num2;
+                numero2 = num;
+            } else {
+                numero1 = num;
+                numero2 = num2;
+            }
+            int datoSalida;
+            Console.Write("El número introducido debe de ser entre {0} y {1}", numero1, numero2);
+            do {   
+                datoSalida = PedirInt();
+                if (datoSalida<= numero2 && datoSalida>= numero1 ) {
+                    salida = true;
+                } else {
+                    Console.Write("El numero debe estar en el rango");
+                }
+            } while (!salida);
+            return datoSalida;
+        }
+        public int PedirInt() {
+            bool salida = false;
+            int numero = 0;
+            do {
+                try {
+                    Console.Write( "\n-> " );
+                    numero = Int32.Parse( Console.ReadLine() );
+                    salida = true;
+                } catch {
+                    Console.Write( "El numero que sea valido" );
+                }
+            } while ( !salida );
+
+            return numero;
+        }
+
+        public int PedirInt(String frase) {
+            Console.Write( frase );
+            return PedirInt();
+        }
+
+        public double PedirDouble() {
+            bool salida = false;
+            double numero = 0.0;
+            do {
+                try {
+                    Console.Write( "\n-> " );
+                    numero = double.Parse( Console.ReadLine() );
+                    salida = true;
+                } catch {
+                    Console.Write( "El numero que sea valido" );
+                }
+            } while ( !salida );
+
+            return numero;
+        }
+
+        public double PedirDoublePositivo() {
+            bool salida = false;
+            double numero = 0.0;
+            do {
+                numero = PedirDouble();
+                if ( numero > 0.0 ) {
+                    salida = true;
+                } else {
+                    Console.Write( "Debe ser mayor a 0" );
+                }
+            } while ( !salida );
+
+            return numero;
+        }
+
+        public double PedirDoublePositivo(string frase) {
+            Console.Write( frase );
+            return PedirDoublePositivo();
+        }
+
+        public int PedirIntPositivo() {
+            bool salida = false;
+            int numero = 0;
+            do {
+                numero = PedirInt();
+                if (numero >= 18) {
+                    salida = true;
+                } else{
+                    Console.Write( "Debe ser mayor de edad" );
+                }
+            } while ( !salida );
+
+            return numero;
+        }
+
+        public int PedirIntPositivo(string frase) {
+            Console.Write( frase );
+            return PedirIntPositivo();
+        }
+    }
+}
